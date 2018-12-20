1c62a53746ed8a68038c6d7fb08104616a1f1756
Cvanzy <cao.zhi@husky.neu.edu>
2018-12-20 14:58:55 -0500

    Sytle 1
    Log in page/

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,7 +4,11 @@ import { startLogin } from "../action/auth";
 
 const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
-    <button onClick={startLogin}> Login </button>
+    <div className="box-layout_box">
+      <h1 className="box-layout_title"> Cexpenses</h1>
+      <p> Record your expenses</p>
+      <button onClick={startLogin}> Login </button>
+    </div>
   </div>
 );
 
