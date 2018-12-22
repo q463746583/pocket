0bd32d18ef1808df9e5ad0ab40ddd53263a3642f
Cvanzy <cao.zhi@husky.neu.edu>
2018-12-22 12:57:47 -0500

    CSS summary update 2
    Add background and more setting

--- src/components/SummaryAmount.js
+++ src/components/SummaryAmount.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import ExpenseListItem from "./ExpenseListItem";
 import selectExpenses from "../selectors/expenses";
 import numeral from "numeral";
@@ -8,10 +9,21 @@ import TotalAmount from "../selectors/TotalAmount";
 export const Summary = ({ expensesLength, totalAmount }) => {
   const formatTotalAmount = numeral(totalAmount / 100).format("$0,0.00");
   return (
-    <div>
-      <p>
-        Your {expensesLength} Expense totally {formatTotalAmount}
-      </p>
+    <div className="summary-header">
+      <h3> Summary: </h3>
+      <div className="content-container">
+        <p className="summary-header__title">
+          Your <span>{expensesLength}</span>{" "}
+          {expensesLength > 1 ? "expenses" : "expense"} totally{" "}
+          <span>{formatTotalAmount}</span>
+        </p>
+      </div>
+      <div className="summary-header__add">
+        <Link className="button__add" to="/create">
+          {" "}
+          Add Expense{" "}
+        </Link>
+      </div>
     </div>
   );
 };
